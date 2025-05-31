41e9dc19a644a3c43415be169a9f0c00f2ae6f2e
al3x_ <77886473+al3x-xx@users.noreply.github.com>
2025-05-31 17:53:29 +0300

    upload

---
+++ README.md
@@ -0,0 +1,23 @@
+Requirement installed Creative Cloud
+
+Example run with command
+```Console
+powershell -ExecutionPolicy Bypass .\main.ps1
+```
+or context menu Run with PowerShell
+
+![](png.png)
+
+if something goes wrong during installation, you can close the Set-up.exe process and delete unnecessary temporary files in the adobeTemp and Temp folders
+
+Microsoft Visual C++ Redistributable Packages not downloaded from the installer. if adobe app does not start, you can install them here https://learn.microsoft.com/cpp/windows/latest-supported-vc-redist
+
+# Thanks
+https://gist.github.com/ayyybe/a5f01c6f40020f9a7bc4939beeb2df1d \
+[https://github.com/ckamte/adobe-packager-windows/blob/main/ccdl-win.py](https://github.com/ckamte/adobe-packager-windows/blob/b91357bfac574b072c6460add2afd2e6857ba5b0/ccdl-win.py)\
+[https://github.com/TsudaKageyu/minhook](https://github.com/TsudaKageyu/minhook/tree/565968b28583221751cc2810e09ea621745fc3a3)\
+IDA \
+x64dbg \
+Cheat Engine \
+Visual Studio \
+Notepad++
---
+++ main.ps1
@@ -0,0 +1,383 @@
+# github.com/al3x-xx/adobe-installer
+
+$data = @(
+    @{
+        Name = 'Photoshop 2024'
+        SAPCode = 'PHSP'
+        BaseVersion = '25.0'
+        CodexVersion = '25.12.3'
+        Platform = 'win64'
+        BuildVersion = '25.12.3.1067'
+        BuildGuid = '922a05d2-de99-40bc-b54e-0f95745f9e2f'
+        SupportedLanguages = 'ar_AE', 'cs_CZ', 'da_DK', 'de_DE', 'en_AE', 'en_GB', 'en_IL', 'en_US', 'en_XM', 'es_ES', 'es_MX', 'fi_FI', 'fr_CA', 'fr_FR', 'fr_MA', 'fr_XM', 'he_IL', 'hu_HU', 'id_ID', 'it_IT', 'ja_JP', 'ko_KR', 'nb_NO', 'nl_NL', 'no_NO', 'pl_PL', 'pt_BR', 'ru_RU', 'sl_SI', 'sv_SE', 'tr_TR', 'uk_UA', 'vi_VN', 'zh_CN', 'zh_TW'
+        Dependencies = @(
+            @{
+                SAPCode = 'ACR'
+                BuildVersion = '17.3.1.3'
+                CodexVersion = '17.3.1'
+                Platform = 'win32'
+                BuildGuid = '84040fd2-a229-439c-a94a-506b4788af95'
+            }
+            @{
+                SAPCode = 'CCXP'
+                BuildVersion = '6.4.1.2'
+                CodexVersion = '6.4.1'
+                Platform = 'win32'
+                BuildGuid = '6ef946ea-f184-4803-89be-c2e736574735'
+            }
+            @{
+                SAPCode = 'COCM'
+                BuildVersion = '1.0.4'
+                CodexVersion = '1.0'
+                Platform = 'win32'
+                BuildGuid = '3fc3cd37-c3fa-11e9-b9c5-12148b63a89a'
+            }
+            @{
+                SAPCode = 'COPS'
+                BuildVersion = '1.0.5'
+                CodexVersion = '1.0'
+                Platform = 'win32'
+                BuildGuid = '3fc3e192-c3fa-11e9-b9c5-12148b63a89a'
+            }
+            @{
+                SAPCode = 'CORE'
+                BuildVersion = '1.0.12'
+                CodexVersion = '1.0'
+                Platform = 'win32'
+                BuildGuid = '3fc3d70f-c3fa-11e9-b9c5-12148b63a89a'
+            }
+            @{
+                SAPCode = 'CORG'
+                BuildVersion = '1.1.4'
+                CodexVersion = '1.1'
+                Platform = 'win32'
+                BuildGuid = '3fd039c5-c3fa-11e9-b9c5-12148b63a89a'
+            }
+            @{
+                SAPCode = 'COSY'
+                BuildVersion = '7.4.0.31'
+                CodexVersion = '7.4.0'
+                Platform = 'win32'
+                BuildGuid = 'f7e2264d-ec70-48ed-9bfa-6125049c98fc'
+            }
+            @{
+                SAPCode = 'UXPW'
+                BuildVersion = '1.3.0.32'
+                CodexVersion = '1.3.0'
+                Platform = 'win64'
+                BuildGuid = '3d3e85bc-382e-4668-9872-d8250d0e7f71'
+            }
+        )
+    }
+    @{
+        Name = 'Premiere Pro 2023'
+        SAPCode = 'PPRO'
+        BaseVersion = '23.0'
+        CodexVersion = '23.6.9'
+        Platform = 'win64'
+        BuildVersion = '23.6.9.4'
+        BuildGuid = '67d1985d-f0d4-44d8-afdc-fd4a975ad5db'
+        SupportedLanguages = 'en_US', 'en_GB', 'en_AE', 'en_IL', 'de_DE', 'es_ES', 'es_MX', 'fr_FR', 'fr_CA', 'fr_MA', 'it_IT', 'ja_JP', 'ko_KR', 'zh_CN', 'pt_BR', 'ru_RU'
+        Dependencies = @(
+            @{
+                SAPCode = 'CCXP'
+                BuildVersion = '6.4.1.2'
+                CodexVersion = '6.4.1'
+                Platform = 'win32'
+                BuildGuid = '6ef946ea-f184-4803-89be-c2e736574735'
+            }
+        )
+    }
+    @{
+        Name = 'After Effects 2023'
+        SAPCode = 'AEFT'
+        BaseVersion = '23.0'
+        CodexVersion = '23.6.9'
+        Platform = 'win64'
+        BuildVersion = '23.6.9.2'
+        BuildGuid = '24af65fc-df33-4b4c-a7a1-0b479b3d278b'
+        SupportedLanguages = 'en_US', 'en_GB', 'en_AE', 'en_IL', 'de_DE', 'es_ES', 'es_MX', 'fr_FR', 'fr_CA', 'fr_MA', 'it_IT', 'ja_JP', 'ko_KR', 'zh_CN', 'pt_BR', 'ru_RU'
+        Dependencies = @(
+            @{
+                SAPCode = 'COMP'
+                BuildVersion = '1.4.1'
+                CodexVersion = '1.4'
+                Platform = 'win32'
+                BuildGuid = '733b0ebc-fdd3-4fef-8f3a-1693e86a756b'
+            }
+            @{
+                SAPCode = 'CORG'
+                BuildVersion = '1.1.4'
+                CodexVersion = '1.1'
+                Platform = 'win32'
+                BuildGuid = '3fd039c5-c3fa-11e9-b9c5-12148b63a89a'
+            }
+            @{
+                SAPCode = 'ACR'
+                BuildVersion = '17.3.1.3'
+                CodexVersion = '17.3.1'
+                Platform = 'win32'
+                BuildGuid = '84040fd2-a229-439c-a94a-506b4788af95'
+            }
+            @{
+                SAPCode = 'CCXP'
+                BuildVersion = '6.4.1.2'
+                CodexVersion = '6.4.1'
+                Platform = 'win32'
+                BuildGuid = '6ef946ea-f184-4803-89be-c2e736574735'
+            }
+        )
+    }
+    @{
+        Name = 'InDesign 2024'
+        SAPCode = 'IDSN'
+        BaseVersion = '19.0'
+        CodexVersion = '19.5.3'
+        Platform = 'win64'
+        BuildVersion = '19.5.3.144'
+        BuildGuid = 'e041b10b-1005-4548-90d8-0bf9205c4baa'
+        SupportedLanguages = 'cs_CZ', 'da_DK', 'de_DE', 'en_AE', 'en_GB', 'en_IL', 'en_US', 'es_ES', 'es_MX', 'fi_FI', 'fr_CA', 'fr_FR', 'fr_MA', 'hu_HU', 'it_IT', 'ja_JP', 'ko_KR', 'nb_NO', 'nl_NL', 'pl_PL', 'pt_BR', 'ru_RU', 'sv_SE', 'tr_TR', 'uk_UA', 'zh_CN', 'zh_TW'
+        Dependencies = @(
+            @{
+                SAPCode = 'CCXP'
+                BuildVersion = '6.4.1.2'
+                CodexVersion = '6.4.1'
+                Platform = 'win32'
+                BuildGuid = '6ef946ea-f184-4803-89be-c2e736574735'
+            }
+            @{
+                SAPCode = 'COCM'
+                BuildVersion = '1.0.4'
+                CodexVersion = '1.0'
+                Platform = 'win32'
+                BuildGuid = '3fc3cd37-c3fa-11e9-b9c5-12148b63a89a'
+            }
+            @{
+                SAPCode = 'CORE'
+                BuildVersion = '1.0.12'
+                CodexVersion = '1.0'
+                Platform = 'win32'
+                BuildGuid = '3fc3d70f-c3fa-11e9-b9c5-12148b63a89a'
+            }
+            @{
+                SAPCode = 'CORG'
+                BuildVersion = '1.1.4'
+                CodexVersion = '1.1'
+                Platform = 'win32'
+                BuildGuid = '3fd039c5-c3fa-11e9-b9c5-12148b63a89a'
+            }
+            @{
+                SAPCode = 'UXPW'
+                BuildVersion = '1.3.0.32'
+                CodexVersion = '1.3.0'
+                Platform = 'win64'
+                BuildGuid = '3d3e85bc-382e-4668-9872-d8250d0e7f71'
+            }
+        )
+    }
+    @{
+        Name = 'Media Encoder 2023'
+        SAPCode = 'AME'
+        BaseVersion = '23.0'
+        CodexVersion = '23.6.9'
+        Platform = 'win64'
+        BuildVersion = '23.6.9.4'
+        BuildGuid = '687a6cd7-30dd-4e7c-ac80-a548065cebd1'
+        SupportedLanguages = 'cs_CZ', 'da_DK', 'de_DE', 'el_GR', 'en_AE', 'en_GB', 'en_IL', 'en_US', 'es_ES', 'es_MX', 'fi_FI', 'fil_PH', 'fr_CA', 'fr_FR', 'fr_MA', 'hi_IN', 'hu_HU', 'id_ID', 'it_IT', 'ja_JP', 'ko_KR', 'ms_MY', 'nb_NO', 'nl_NL', 'pl_PL', 'pt_BR', 'ro_RO', 'ru_RU', 'sv_SE', 'th_TH', 'tr_TR', 'uk_UA', 'vi_VN', 'zh_CN', 'zh_TW'
+        SkipLanguageCheck = $true
+    }
+    @{
+        Name = 'Audition 2023'
+        SAPCode = 'AUDT'
+        BaseVersion = '23.0'
+        CodexVersion = '23.6.9'
+        Platform = 'win64'
+        BuildVersion = '23.6.9.4'
+        BuildGuid = '3512f9aa-9ea4-43c1-99af-e353b5f8f090'
+        SupportedLanguages = 'en_US', 'en_GB', 'en_AE', 'en_IL', 'de_DE', 'es_ES', 'es_MX', 'fr_FR', 'fr_CA', 'fr_MA', 'it_IT', 'ja_JP', 'ko_KR', 'zh_CN', 'pt_BR'
+    }
+    @{
+        Name = 'Lightroom Classic 2023'
+        SAPCode = 'LTRM'
+        BaseVersion = '8.3' # wtf
+        CodexVersion = '13.5.1'
+        Platform = 'win64'
+        BuildVersion = '13.5.1.202408271704'
+        BuildGuid = 'c13bd591-e43d-4670-b506-e56170d65de9'
+        SupportedLanguages = 'de_DE', 'en_US', 'es_ES', 'fr_FR', 'it_IT', 'ja_JP', 'ko_KR', 'nb_NO', 'nl_NL', 'pl_PL', 'pt_BR', 'ru_RU', 'sv_SE', 'th_TH', 'zh_CN', 'zh_TW'
+    }
+    @{
+        Name = 'InCopy 2024'
+        SAPCode = 'AICY'
+        BaseVersion = '19.0'
+        CodexVersion = '19.5.3'
+        Platform = 'win64'
+        BuildVersion = '19.5.3.144'
+        BuildGuid = '5ea67b51-c5bb-4552-9d55-7bde907de29c'
+        SupportedLanguages = 'cs_CZ', 'da_DK', 'de_DE', 'en_AE', 'en_GB', 'en_IL', 'en_US', 'es_ES', 'es_MX', 'fi_FI', 'fr_CA', 'fr_FR', 'fr_MA', 'hu_HU', 'it_IT', 'ja_JP', 'ko_KR', 'nb_NO', 'nl_NL', 'pl_PL', 'pt_BR', 'ru_RU', 'sv_SE', 'tr_TR', 'uk_UA', 'zh_CN', 'zh_TW'
+        Dependencies = @(
+            @{
+                SAPCode = 'CCXP'
+                BuildVersion = '6.4.1.2'
+                CodexVersion = '6.4.1'
+                Platform = 'win32'
+                BuildGuid = '6ef946ea-f184-4803-89be-c2e736574735'
+            }
+            @{
+                SAPCode = 'COCM'
+                BuildVersion = '1.0.4'
+                CodexVersion = '1.0'
+                Platform = 'win32'
+                BuildGuid = '3fc3cd37-c3fa-11e9-b9c5-12148b63a89a'
+            }
+            @{
+                SAPCode = 'CORE'
+                BuildVersion = '1.0.12'
+                CodexVersion = '1.0'
+                Platform = 'win32'
+                BuildGuid = '3fc3d70f-c3fa-11e9-b9c5-12148b63a89a'
+            }
+            @{
+                SAPCode = 'CORG'
+                BuildVersion = '1.1.4'
+                CodexVersion = '1.1'
+                Platform = 'win32'
+                BuildGuid = '3fd039c5-c3fa-11e9-b9c5-12148b63a89a'
+            }
+            @{
+                SAPCode = 'UXPW'
+                BuildVersion = '1.3.0.32'
+                CodexVersion = '1.3.0'
+                Platform = 'win64'
+                BuildGuid = '3d3e85bc-382e-4668-9872-d8250d0e7f71'
+            }
+        )
+    }
+    @{
+        Name = 'Character Animator 2023'
+        SAPCode = 'CHAR'
+        BaseVersion = '23.0'
+        CodexVersion = '23.6'
+        Platform = 'win64'
+        BuildVersion = '23.6.0.58'
+        BuildGuid = 'a391af14-7fbd-419e-89be-696099dd8cb9'
+        SupportedLanguages = 'en_US', 'en_GB', 'en_AE', 'en_IL', 'es_ES', 'es_MX', 'de_DE', 'fr_FR', 'fr_CA', 'fr_MA', 'it_IT', 'ja_JP', 'ko_KR', 'pt_BR', 'ru_RU', 'zh_CN'
+    }
+    @{
+        Name = 'Bridge 2024'
+        SAPCode = 'KBRG'
+        BaseVersion = '14.0.0'
+        CodexVersion = '14.1.7'
+        Platform = 'win64'
+        BuildVersion = '14.1.7.321'
+        BuildGuid = 'be3acab5-0cd1-454a-bac4-c55ee24ed72a'
+        SupportedLanguages = 'cs_CZ', 'da_DK', 'de_DE', 'en_AE', 'en_GB', 'en_IL', 'en_US', 'es_ES', 'es_MX', 'fi_FI', 'fr_CA', 'fr_FR', 'fr_MA', 'hu_HU', 'it_IT', 'ja_JP', 'ko_KR', 'nb_NO', 'nl_NL', 'pl_PL', 'pt_BR', 'ru_RU', 'sv_SE', 'tr_TR', 'uk_UA', 'zh_CN', 'zh_TW'
+        Dependencies = @(
+            @{
+                SAPCode = 'ACR'
+                BuildVersion = '17.3.1.3'
+                CodexVersion = '17.3.1'
+                Platform = 'win32'
+                BuildGuid = '84040fd2-a229-439c-a94a-506b4788af95'
+            }
+        )
+    }
+)
+
+$data = & {
+    for ($i = 0; $i -lt $data.Length; $i++) {
+        Write-Host "[$i] $($data[$i].Name) $($data[$i].BuildVersion)"
+    }
+
+    if ($data = $data[[int](Read-Host 'Enter value from brackets | Example: 0 ')]) {
+        $data
+    } else {
+        throw 'Error out of range'
+    }
+}
+
+if ($data.SkipLanguageCheck) {
+    $language = $data.SupportedLanguages -join ','
+} else {
+    Write-Host $data.SupportedLanguages -Separator ', '
+
+    if ($data.SupportedLanguages -cnotcontains ($language = Read-Host 'Enter language | Example: aa_AA ')) {
+        return 'Error language not found'
+    }
+}
+
+$xml = & {
+    foreach ($buf in $data.Dependencies) {
+        # buf.BuildGuid buf.BuildVersion buf.CodexVersion buf.Platform buf.SAPCode
+        $res += "&amp;lt;Dependency&amp;gt;&amp;lt;BuildGuid&amp;gt;$($buf.BuildGuid)&amp;lt;/BuildGuid&amp;gt;&amp;lt;BuildVersion&amp;gt;$($buf.BuildVersion)&amp;lt;/BuildVersion&amp;gt;&amp;lt;CodexVersion&amp;gt;$($buf.CodexVersion)&amp;lt;/CodexVersion&amp;gt;&amp;lt;Platform&amp;gt;$($buf.Platform)&amp;lt;/Platform&amp;gt;&amp;lt;SAPCode&amp;gt;$($buf.SAPCode)&amp;lt;/SAPCode&amp;gt;&amp;lt;/Dependency&amp;gt;"
+    }
+    # data.Name data.SAPCode data.CodexVersion data.BaseVersion data.Platform data.BuildGuid data.BuildVersion
+    # &amp;lt;SystemRequirement&amp;gt;&amp;lt;MicrosoftVcRuntimeList&amp;gt;&amp;lt;win32&amp;gt;VC_,VC_._&amp;lt;/win32&amp;gt;&amp;lt;win64&amp;gt;VC_,VC_._&amp;lt;/win64&amp;gt;&amp;lt;/MicrosoftVcRuntimeList&amp;gt;&amp;lt;/SystemRequirement&amp;gt;
+    "<BootStrapMsg><MsgName>installHDProduct</MsgName><MsgData>&lt;messageData&gt;&lt;driverXMLData&gt;&amp;lt;?xml version='1.0' standalone='yes'?&amp;gt;&amp;lt;DriverInfo&amp;gt;&amp;lt;ProductInfo&amp;gt;&amp;lt;Name&amp;gt;$($data.Name)&amp;lt;/Name&amp;gt;&amp;lt;SAPCode&amp;gt;$($data.SAPCode)&amp;lt;/SAPCode&amp;gt;&amp;lt;CodexVersion&amp;gt;$($data.CodexVersion)&amp;lt;/CodexVersion&amp;gt;&amp;lt;BaseVersion&amp;gt;$($data.BaseVersion)&amp;lt;/BaseVersion&amp;gt;&amp;lt;Dependencies&amp;gt;$res&amp;lt;/Dependencies&amp;gt;&amp;lt;IsNonCCProduct&amp;gt;false&amp;lt;/IsNonCCProduct&amp;gt;&amp;lt;IsNglEnabled&amp;gt;true&amp;lt;/IsNglEnabled&amp;gt;&amp;lt;SupportedLanguages&amp;gt;&amp;lt;/SupportedLanguages&amp;gt;&amp;lt;Platform&amp;gt;$($data.Platform)&amp;lt;/Platform&amp;gt;&amp;lt;BuildGuid&amp;gt;$($data.BuildGuid)&amp;lt;/BuildGuid&amp;gt;&amp;lt;BuildVersion&amp;gt;$($data.BuildVersion)&amp;lt;/BuildVersion&amp;gt;&amp;lt;/ProductInfo&amp;gt;&amp;lt;/DriverInfo&amp;gt;&lt;/driverXMLData&gt;&lt;installLanguage&gt;$language&lt;/installLanguage&gt;&lt;installLocation&gt;&lt;/installLocation&gt;&lt;wamMode&gt;true&lt;/wamMode&gt;&lt;/messageData&gt;</MsgData></BootStrapMsg>`0"
+}
+
+$pipeName = ([string](New-Guid)).ToUpper()
+
+$pipeOut = [System.IO.Pipes.NamedPipeServerStream]::new("{$pipeName}_OUT", [System.IO.Pipes.PipeDirection]::Out, 1, [System.IO.Pipes.PipeTransmissionMode]::Byte, [System.IO.Pipes.PipeOptions]::None, 0x3E8000, 0x3E8000)
+$pipeIn = [System.IO.Pipes.NamedPipeServerStream]::new("{$pipeName}_IN", [System.IO.Pipes.PipeDirection]::In, 1, [System.IO.Pipes.PipeTransmissionMode]::Byte, [System.IO.Pipes.PipeOptions]::None, 0x3E8000, 0x3E8000)
+
+if (!(Test-Path ($filePath = [System.Environment]::GetEnvironmentVariable('ProgramFiles(x86)') + '\Common Files\Adobe\Adobe Desktop Common\HDBox\Set-up.exe'))) {
+    return 'Error Set-up.exe not found'
+}
+
+try {
+    $process = Start-Process $filePath ("--pipename={$pipeName}" , '--edtWorkFlow=1') -PassThru -Verb RunAs
+} catch {
+    return 'Error Start-Process'
+}
+
+'WaitForConnection'
+
+$pipeOut.WaitForConnection()
+$pipeIn.WaitForConnection()
+
+'Connected'
+
+$out = [System.IO.BinaryWriter]::new($pipeOut)
+$out.Write([byte[]](0xA, 0x0, 0x0, 0x0, 0xFF, 0xFF, 0x0, 0x0 + [System.BitConverter]::GetBytes($xml.Length)))
+$out.Write([System.Text.Encoding]::ASCII.GetBytes($xml))
+
+'Wait'
+
+$in = [System.IO.BinaryReader]::new($pipeIn)
+
+try {
+    :exit_ while ($true) {
+        if ($in.ReadInt64() -ne 0xFFFF0000000A) { break }
+
+        $length = $in.ReadInt32()
+
+        if (($xml = [System.Text.Encoding]::ASCII.GetString($in.ReadBytes($length))) -notmatch '<MsgName>(.+?)</MsgName>') { break }
+        
+        switch ($Matches[1]) {
+            'hdInstallProgress' {
+                if ($xml -match 'progressPercentage&gt;([0-9]+\.[0-9]+|[0-9]+?)&lt') {
+                    Write-Host "`rDownloading / Installing $([System.Math]::Ceiling($Matches[1]))%" -NoNewline
+                } else {
+                    Write-Host "`rDownloading / Installing     " -NoNewline
+                }
+                break
+            }
+            'hdInstallComplete' {
+                "`rDownloading / Installing 100%"
+                break exit_
+            }
+            'hdInstallError' {
+                "`rhdInstallError | maybe already installing/installed / install/reinstall Creative Cloud"
+                break exit_
+            }
+        }
+    }
+} catch {}
+
+$in.Close()
+$out.Close()
+
+Stop-Process $process
+
+pause
